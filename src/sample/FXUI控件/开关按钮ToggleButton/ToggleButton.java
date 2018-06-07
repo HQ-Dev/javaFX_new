de77fc176ddbbc86844b025c1e6281c3656022f9
Archy <hq_dev@163.com>
2018-06-07 13:56:11 +0800

    update

---
+++ src/sample/FXUI控件/开关按钮ToggleButton/ToggleButton.java
@@ -0,0 +1,8 @@
+package sample.FXUI控件.开关按钮ToggleButton;
+
+/**
+ * @author Archy
+ * @date Created at 2018/6/7
+ **/
+public class ToggleButton {
+}
--- src/swingInJavaFX/SwingFx.java
+++ src/swingInJavaFX/SwingFx.java
@@ -1,8 +1,34 @@
 package swingInJavaFX;
 
+import javafx.application.Application;
+import javafx.embed.swing.SwingNode;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import javax.swing.*;
+
 /**
  * @author Archy
  * @date Created at 2018/5/5
  **/
-public class SwingFx {
+public class SwingFx extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final SwingNode swingNode = new SwingNode();
+        createSwingNodeContent(swingNode);
+
+        StackPane stackPane = new StackPane();
+        stackPane.getChildren().add(swingNode);
+
+        primaryStage.setTitle("Swing in JavaFX");
+        primaryStage.setScene(new Scene(stackPane, 300, 350));
+        primaryStage.show();
+    }
+
+    private void createSwingNodeContent(final SwingNode swingNode) {
+        SwingUtilities.invokeLater(() -> {
+            swingNode.setContent(new JButton("swing button: click me!"));
+        });
+    }
 }
